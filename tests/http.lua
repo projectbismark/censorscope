6a3b0f13eb3e5f8e8d42c7306ebbe934103b00c5
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2013-09-23 17:07:19 -0400

    Add some structure to the http test

--- tests/http.lua
+++ tests/http.lua
@@ -1,10 +1,27 @@
-local http = require("socket.http")
+http = require("socket.http")
 
-body, status_code, header, status_line = http.request("http://www.google.com")
+HttpRequestExperiment = {}
+HttpRequestExperiment.__index = HttpRequestExperiment
 
-print("Body")
-print(body)
-print("Status Code:")
-print(status_code)
-print("Headers")
-for k,v in pairs(header) do print (k,v) end
+function HttpRequestExperiment.create(urls)
+   local experiment = {}
+   setmetatable(experiment, HttpRequestExperiment)
+   experiment.urls = urls
+   return experiment
+end
+
+function HttpRequestExperiment:run()
+   for k, url in pairs(self.urls) do
+      print(k, url)
+      body, status_code, header, status_line = http.request(url)
+      print("Body:")
+      print(body)
+      print("Status Code:")
+      print(status_code)
+      print("Headers")
+      for k,v in pairs(header) do print (k,v) end
+   end
+end
+
+experiment = HttpRequestExperiment.create({"http://www.google.com"})
+experiment:run()
