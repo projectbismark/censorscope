df82d164771a9758ebe4e5417eff2f1ca97e5805
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2013-09-26 16:12:35 -0400

    Rewrite tcpconnect test
    Remove OOP, Add comments

--- tests/tcpconnect.lua
+++ tests/tcpconnect.lua
@@ -1,26 +1,56 @@
 local socket = require("socket")
+local utils = require("utils")
 
-TcpConnectExperiment = {}
-TcpConnectExperiment.__index = TcpConnectExperiment
+function TcpConnectExperiment(urls)
+   -- Store results of all the TCP Requests
+   local results = {}
+   
+   for _, url in ipairs(urls) do
+      -- Parse the url to get host, port
+      -- TODO: Wrap this around pcall()
+      host, port = parse_url(url)
 
-function TcpConnectExperiment.create(uris)
-   local experiment = {}
-   setmetatable(experiment, TcpConnectExperiment)
-   experiment.uris = uris
-   return experiment
-end
-
-function TcpConnectExperiment:run()
-   for host, port in pairs(self.uris) do
+      -- Do a TCP Connect
       client, msg = socket.connect(host, port)
+
+      -- Store the result in a table.
+      -- This "result" table has only 
+      -- information about a single TCP Request.
+      local result = {}
+
       if client then
-         print("Success")
+         result.success = true
       else
-         print("Failure", msg)
-      end
+         result.success = false
+
+      result.url = url
+      result.msg = msg
+
+      -- Add this table to our list of results
+      table.insert(results, result)
    end
 end
 
-uris = {["74.125.140.113"] = "80"}
-experiment = TcpConnectExperiment.create(uris)
-experiment:run()
+function parse_url(url)
+   -- This function assumes the url is of
+   -- the form "<host>:<port>", if not raise
+   -- an error.
+   
+   -- Check if there is a ":" separator
+   found = string.find(url, ":")
+
+   if found ~= nil then
+      -- Split the url into host and port
+      uri = utils.split(url, ":")
+
+      -- Unpack the uri table and return the values
+      return uri[1], uri[2]
+   else
+      -- Raise an error if URL is not formatted
+      -- correctly.
+      error("Require URL of the format '<host>:<ip'")
+   end
+end
+
+urls = {"74.125.140.113:80"}
+TcpConnectExperiment(urls)
