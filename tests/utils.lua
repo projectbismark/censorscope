8529f3cbb8b2ad3c94602cdef04fa5b3447de478
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2013-09-26 16:12:07 -0400

    Add utils
    We need misc util functions like split which
    are missing from the lua standard library.

---
+++ tests/utils.lua
@@ -0,0 +1,26 @@
+-- Table to store all the util functions
+-- we define here.
+local utils = {}
+
+-- See http://lua-users.org/wiki/SplitJoin
+-- for more information
+function utils.split(str, pat)
+   local t = {}  -- NOTE: use {n = 0} in Lua-5.0
+   local fpat = "(.-)" .. pat
+   local last_end = 1
+   local s, e, cap = str:find(fpat, 1)
+   while s do
+      if s ~= 1 or cap ~= "" then
+	 table.insert(t,cap)
+      end
+      last_end = e+1
+      s, e, cap = str:find(fpat, last_end)
+   end
+   if last_end <= #str then
+      cap = str:sub(last_end)
+      table.insert(t, cap)
+   end
+   return t
+end
+
+return utils
