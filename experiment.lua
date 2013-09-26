3daff48372eabb049938024f8f6beb3c94a51a5c
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2013-09-26 19:01:32 -0400

    Add a director
    The director runs all the experiments and outputs the results.

---
+++ experiment.lua
@@ -0,0 +1,22 @@
+utils = require("tests.utils")
+
+local tcpconnect = {}
+tcpconnect.urls = {"74.125.140.113:80"}
+
+local http = {}
+http.urls = {"http://www.google.com"}
+
+local experiments = {}
+experiments.http = http
+experiments.tcpconnect = tcpconnect
+
+for name, experiment in pairs(experiments) do
+   experiment.exec = require("tests."..name)
+   local results = experiment.exec(experiment.urls)
+
+   for _, result in pairs(results) do
+      for key, value in pairs(result) do
+         print(key, value)
+      end
+   end
+end
