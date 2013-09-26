4a75e23c13877ea31fceaf8c967a61273b0c3a27
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2013-09-26 16:11:46 -0400

    Refactor http.lua

--- tests/http.lua
+++ tests/http.lua
@@ -1,25 +1,30 @@
 local http = require("socket.http")
 
-HttpRequestExperiment = {}
-HttpRequestExperiment.__index = HttpRequestExperiment
-
-function HttpRequestExperiment.create(urls)
-   local experiment = {}
-   setmetatable(experiment, HttpRequestExperiment)
-   experiment.urls = urls
-   return experiment
-end
-
-function HttpRequestExperiment:run()
-   for _, url in pairs(self.urls) do
+function HttpRequestExperiment(urls)
+   -- Store results of all the HTTP Requests.
+   local results = {}
+   
+   for _, url in ipairs(urls) do
+      -- Make a HTTP request
       body, status_code, header, status_line = http.request(url)
-      if status_code == 200 then
-         print("Url:", url)
-         print("Status: ", status_code)
-      end
+
+      -- Store the result in a table.
+      -- This "result" table has only 
+      -- information about a single HTTP Request.
+      local result = {}
+      result.url = url
+      result.body = body
+      result.status_code = status_code
+      result.header = header
+      result.status_line = status_line
+
+      -- Add this table to our list of results
+      table.insert(results, result)
    end
+
+   -- Return our list of all results
+   return results
 end
 
 urls = {"http://www.google.com"}
-experiment = HttpRequestExperiment.create(urls)
-experiment:run()
+res = HttpRequestExperiment(urls)
