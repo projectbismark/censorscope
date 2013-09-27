0920baad313f9dc168b4defd9a34d4ecb64f2f03
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2013-09-27 16:35:42 -0400

    Read input from file
    get_input reads input files from the inputs/ folder

--- censorscope.lua
+++ censorscope.lua
@@ -1,15 +1,31 @@
 local tcp_connect = {}
-tcp_connect.urls = {"74.125.140.113:80"}
 
 local http_request = {}
-http_request.urls = {"http://www.google.com"}
 
 local experiments = {}
 experiments.http_request = http_request
 experiments.tcp_connect = tcp_connect
 
+function get_input(file_name)
+   -- Local table to store list of inputs
+   local inputs = {}
+
+   -- Open file to read from
+   io.input("inputs/"..file_name)
+
+   -- Iterate through the file and
+   -- store it in "inputs" table
+   for line in io.lines() do
+      table.insert(inputs, line)
+   end
+
+   -- Return the list of inputs
+   return inputs
+end
+
 for name, experiment in pairs(experiments) do
    experiment.exec = require("experiments."..name)
+   experiment.urls = get_input(name..".txt")
    local results = experiment.exec(experiment.urls)
 
    for _, result in pairs(results) do
