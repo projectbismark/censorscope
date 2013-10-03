72aab8d60ebab47bc1a516e98a12c7e89c599564
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2013-10-03 14:36:51 -0400

    Change indentation to 2 chars

--- censorscope.lua
+++ censorscope.lua
@@ -5,41 +5,41 @@ local utils = require("experiments.utils")
 local experiments = require("config")
 
 function get_input(file_name)
-   -- Local table to store list of inputs
-   local inputs = {}
+  -- Local table to store list of inputs
+  local inputs = {}
 
-   -- Open file to read from
-   -- TODO: Check if file exists
-   io.input("inputs/"..file_name)
+  -- Open file to read from
+  -- TODO: Check if file exists
+  io.input("inputs/"..file_name)
 
-   -- Iterate through the file and store
-   -- it in "inputs" table
-   for line in io.lines() do
-      table.insert(inputs, line)
-   end
+  -- Iterate through the file and store
+  -- it in "inputs" table
+  for line in io.lines() do
+     table.insert(inputs, line)
+  end
 
-   -- Return the list of inputs
-   return inputs
+  -- Return the list of inputs
+  return inputs
 end
 
 function write_result(result, file_name)
-   -- TODO: Check if file already exists
-   -- TODO: Use timestamps in file_name?
-   -- TODO: Check if results/ is present, if not
-   --       create it
-   io.output("results/"..file_name)
+  -- TODO: Check if file already exists
+  -- TODO: Use timestamps in file_name?
+  -- TODO: Check if results/ is present, if not
+  --       create it
+  io.output("results/"..file_name)
 
-   -- Convert tables to string and write to file
-   utils.serialize(result)
+  -- Convert tables to string and write to file
+  utils.serialize(result)
 end
 
 for name, experiment in pairs(experiments) do
-   experiment.exec = require("experiments."..name)
-   experiment.urls = get_input(experiment.input)
+  experiment.exec = require("experiments."..name)
+  experiment.urls = get_input(experiment.input)
 
-   local results = experiment.exec(experiment.urls)
+  local results = experiment.exec(experiment.urls)
 
-   for _, result in pairs(results) do
-      write_result(result, experiment.output)
-   end
+  for _, result in pairs(results) do
+    write_result(result, experiment.output)
+  end
 end
