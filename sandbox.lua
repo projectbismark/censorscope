cd21eb1ca02b78e528f03111362836667b0f1d3c
Sam Burnett <srburnet@gmail.com>
2013-11-06 18:41:50 -0500

    Add a simple sandbox for running experiments.
    We do not expose any functions to the sandbox, so it's pretty useless right now.

---
+++ sandbox.lua
@@ -0,0 +1,48 @@
+local sandbox = {}
+
+local BYTECODE_MAGIC_NUMBER = 27
+
+local sandboxed_environment = {}
+
+local function load_sandboxed_code(filename)
+  local handle, err = io.open(filename, "r")
+  if err then
+    return nil, err
+  end
+  local sandboxed_code = handle:read("*a")  -- "*a" means "read the whole file."
+  if not sandboxed_code then
+    return nil, "error reading contents of file " .. filename
+  end
+  if sandboxed_code:byte(1) == BYTECODE_MAGIC_NUMBER then
+    return nil, "cannot execute binary bytecode in the sandbox"
+  end
+  local sandboxed_function, err = loadstring(sandboxed_code)
+  if err then
+    return nil, err
+  end
+  return sandboxed_function, nil
+end
+
+-- Run an experiment in a sandbox.
+--
+-- See http://lua-users.org/wiki/SandBoxes
+--
+-- Arguments:
+-- - filename is the filename of the Lua source to execute in the sandbox.
+-- Returns:
+-- - the return of evaluation of the code, or nil if execution was unsuccessful.
+-- - an error, or nil if execution was successful.
+function sandbox.run(filename)
+  local sandboxed_function, err = load_sandboxed_code(filename)
+  if err then
+    return nil, err
+  end
+  setfenv(sandboxed_function, sandboxed_environment)
+  local is_successful, return_value = pcall(sandboxed_function)
+  if not is_successful then
+    return nil, "error running sandboxed code: " .. return_value
+  end
+  return return_value, nil
+end
+
+return sandbox
