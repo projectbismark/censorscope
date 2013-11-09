502c7f30ea25bea9644e97fd87d66718aeb607bb
Sam Burnett <srburnet@gmail.com>
2013-11-08 19:10:45 -0500

    Add comment about Alexa 100.

--- sandbox/alexa_100.lua
+++ sandbox/alexa_100.lua
@@ -1,3 +1,9 @@
+-- These are the 100 most popular domain names according to Alexa on 2013-11-08
+-- See http://www.alexa.com/topsites.
+--
+-- Note that some of these sites contain adult content, so it might not be
+-- appropriate to run these in some locations.
+
 return {
   "google.com",
   "facebook.com",
