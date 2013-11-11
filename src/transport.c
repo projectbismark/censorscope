e5bb31272f37b0fb989ff7c9dc174b5ef5c45d02
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2013-11-10 22:21:17 -0500

    Destroy transport on error

--- src/transport.c
+++ src/transport.c
@@ -22,7 +22,7 @@ int transport_init(transport_t *transport, const char *module) {
         fprintf(stderr,
                 "Error loading transport: %s\n",
                 luaL_checkstring(transport->L, -1));
-        lua_close(transport->L);
+        transport_destroy(transport);
         return -1;
     }
 
@@ -35,6 +35,7 @@ int transport_download(transport_t *transport) {
         fprintf(stderr,
                 "Error downloading files: %s\n",
                 luaL_checkstring(transport->L, -1));
+        transport_destroy(transport);
         return -1;
     }
     return 0;
@@ -46,6 +47,7 @@ int transport_upload(transport_t *transport) {
         fprintf(stderr,
                 "Error uploading results: %s\n",
                 luaL_checkstring(transport->L, -1));
+        transport_destroy(transport);
         return -1;
     }
     return 0;
