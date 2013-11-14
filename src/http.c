56e6fc66535d0c02d227a8c53ed64fa0d30cd789
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2013-11-14 17:47:52 -0500

    Use a struct buffer to store the response

--- src/http.c
+++ src/http.c
@@ -10,25 +10,45 @@
 #include "lua.h"
 #include "lauxlib.h"
 
-static size_t write_data(void *ptr, size_t size, size_t nmemb, void *arg)
+struct buffer {
+    char *string;
+    size_t len;
+};
+
+void init_buffer(struct buffer *data) {
+    data->len = 0;
+    data->string = malloc(data->len+1);
+
+    if (data->string == NULL) {
+        fprintf(stderr, "malloc() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    data->string[0] = '\0';
+}
+
+static size_t write_data(void *string, size_t size, size_t nmemb, void *arg)
 {
-    lua_State *L = arg;
+    struct buffer *data = (struct buffer *) arg;
+
     const size_t data_len = size*nmemb;
-    char *response = malloc(data_len + 2);
+    const size_t new_len = data->len + data_len;
 
-    memcpy(response, ptr, data_len);
-    response[data_len] = '\0';
+    data->string = realloc(data->string, new_len+1);
+    if (data->string == NULL) {
+        fprintf(stderr, "realloc failed\n");
+    }
 
-    printf("%s\n",response);
-    lua_pushstring(L, "foo");
+    memcpy(data->string + data->len, string, data_len);
+    data->string[new_len] = '\0';
+    data->len = new_len;
 
-    free(response);
     return data_len;
 }
 
 int l_http_get(lua_State *L) {
     CURL *curl_handle;
     CURLcode res;
+
     const char *url = luaL_checkstring(L, 1);
     fprintf(stderr, "URL - %s\n", url);
 
@@ -41,6 +61,10 @@ int l_http_get(lua_State *L) {
         return 2;
     }
 
+    /* create a buffer to keep our response */
+    struct buffer data;
+    init_buffer(&data);
+
     /* set URL to get */
     curl_easy_setopt(curl_handle, CURLOPT_URL, url);
 
@@ -50,8 +74,8 @@ int l_http_get(lua_State *L) {
     /* send all data to this function  */
     curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
 
-    /* pass the Lua stack to the write function */
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, L);
+    /* pass the response string to the write function */
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, &data);
 
     /* perform the request */
     res = curl_easy_perform(curl_handle);
@@ -62,9 +86,13 @@ int l_http_get(lua_State *L) {
         return 2;
     }
 
+    printf("%d\n", data.len);
+    printf("%s\n", data.string);
+    /* lua_pushstring(L, data.string); */
+
     /* cleanup */
+    free(data.string);
     curl_easy_cleanup(curl_handle);
     lua_pushnil(L);
-
     return 2;
 }
