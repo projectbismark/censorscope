fe46da81f4cb87324e2d005c01e92471e16c49e5
Sam Burnett <srburnet@gmail.com>
2013-11-20 19:56:19 -0500

    Fix bismark uploads path

--- luasrc/transports/bismark.lua
+++ luasrc/transports/bismark.lua
@@ -1,7 +1,7 @@
 local bismark = {}
 
 function bismark.upload_results(results_path)
-  local mv_command = "mv " .. results_path .. "/* /tmp/bismark-uploads"
+  local mv_command = "mv " .. results_path .. "/* /tmp/bismark-uploads/censorscope"
   assert(os.execute(mv_command) == 0,
          "error uploading results using bismark-data-transmit")
 end
