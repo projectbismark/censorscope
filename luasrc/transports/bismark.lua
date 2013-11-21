f34089f92a15ce27ed6915b600e6b881c9cbb04d
Sam Burnett <srburnet@gmail.com>
2013-11-20 19:40:03 -0500

    Add transport for using bismark-data-transmit.

---
+++ luasrc/transports/bismark.lua
@@ -0,0 +1,9 @@
+local bismark = {}
+
+function bismark.upload_results(results_path)
+  local mv_command = "mv " .. results_path .. "/* /tmp/bismark-uploads"
+  assert(os.execute(mv_command) == 0,
+         "error uploading results using bismark-data-transmit")
+end
+
+return bismark
