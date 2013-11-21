77c6aff2b0af68c749c51c8756c833b983bf234a
Sam Burnett <srburnet@gmail.com>
2013-11-20 22:47:53 -0500

    Add bismark download sync

--- luasrc/transports/bismark.lua
+++ luasrc/transports/bismark.lua
@@ -6,4 +6,17 @@ function bismark.upload_results(results_path)
          "error uploading results using bismark-data-transmit")
 end
 
+function bismark.sync_sandbox(sandbox_path)
+  local remote_path = "bismark-downloads.noise.gatech.edu::sandbox"
+  local ssh_command = "ssh -i /etc/bismark/bismark_key -l censorscope"
+  local rsync_command = string.format("rsync --delete -avz -e '%s' %s %s",
+                                      ssh_command,
+                                      remote_path,
+                                      sandbox_path)
+
+  local exit_code = assert(os.execute(rsync_command) == 0,
+                           "error downloading experiments using rsync")
+  return exit_code
+end
+
 return bismark
