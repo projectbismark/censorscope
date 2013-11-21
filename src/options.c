5fe6107f01fe47d103b4f90d27d18302f0e436aa
Sam Burnett <srburnet@gmail.com>
2013-11-20 19:18:35 -0500

    Add a few new command line options.

--- src/options.c
+++ src/options.c
@@ -15,6 +15,10 @@
 #define DEFAULT_LUASRC_DIR "luasrc"
 #endif
 
+#ifndef DEFAULT_RESULTS_DIR
+#define DEFAULT_RESULTS_DIR "results"
+#endif
+
 #ifndef DEFAULT_MAX_MEMORY
 #define DEFAULT_MAX_MEMORY 0
 #endif
@@ -23,21 +27,35 @@
 #define DEFAULT_MAX_INSTRUCTIONS 0
 #endif
 
+#ifndef DEFAULT_DOWNLOAD_TRANSPORT
+#define DEFAULT_DOWNLOAD_TRANSPORT "rsync"
+#endif
+
+#ifndef DEFAULT_UPLOAD_TRANSPORT
+#define DEFAULT_UPLOAD_TRANSPORT "rsync"
+#endif
+
 static void print_usage(const char *program) {
     const char *usage_string =
         "Usage: %s [options]\n"
+        "  -d --download-transport <transport> (default: \"%s\")\n"
         "  -h --help\n"
         "  -i --max-instructions <instructions> (default: %ld)\n"
         "  -l --luasrc-dir <path> (default: \"%s\")\n"
         "  -m --max-memory <bytes> (default: %ld)\n"
-        "  -s --sandbox-dir <path> (default: \"%s\")\n";
+        "  -r --results-dir <path> (default: \"%s\")\n"
+        "  -s --sandbox-dir <path> (default: \"%s\")\n"
+        "  -u --upload-transport <transport> (default: \"%s\")\n";
     fprintf(stderr,
             usage_string,
             program,
+            DEFAULT_DOWNLOAD_TRANSPORT,
             DEFAULT_MAX_INSTRUCTIONS,
             DEFAULT_LUASRC_DIR,
             DEFAULT_MAX_MEMORY,
-            DEFAULT_SANDBOX_DIR);
+            DEFAULT_RESULTS_DIR,
+            DEFAULT_SANDBOX_DIR,
+            DEFAULT_UPLOAD_TRANSPORT);
 }
 
 int censorscope_options_init(censorscope_options_t *options,
@@ -54,16 +72,43 @@ int censorscope_options_init(censorscope_options_t *options,
         perror("stdrup");
         return -1;
     }
+    options->results_dir = strdup(DEFAULT_RESULTS_DIR);
+    if (!options->results_dir) {
+        free(options->luasrc_dir);
+        free(options->sandbox_dir);
+        perror("stdrup");
+        return -1;
+    }
     options->max_memory = DEFAULT_MAX_MEMORY;
     options->max_instructions = DEFAULT_MAX_INSTRUCTIONS;
+    options->download_transport = strdup(DEFAULT_DOWNLOAD_TRANSPORT);
+    if (!options->download_transport) {
+        free(options->results_dir);
+        free(options->luasrc_dir);
+        free(options->sandbox_dir);
+        perror("stdrup");
+        return -1;
+    }
+    options->upload_transport = strdup(DEFAULT_UPLOAD_TRANSPORT);
+    if (!options->upload_transport) {
+        free(options->download_transport);
+        free(options->results_dir);
+        free(options->luasrc_dir);
+        free(options->sandbox_dir);
+        perror("stdrup");
+        return -1;
+    }
 
-    const char *short_options = "hi:l:m:s:";
+    const char *short_options = "d:hi:l:m:r:s:u:";
     const struct option long_options[] = {
+        {"download-transport", 1, NULL, 'd'},
         {"help", 0, NULL, 'h'},
         {"max-instructions", 1, NULL, 'i'},
         {"luasrc-dir", 1, NULL, 'l'},
         {"max-memory", 1, NULL, 'm'},
+        {"results-dir", 1, NULL, 'r'},
         {"sandbox-dir", 1, NULL, 's'},
+        {"upload-transport", 1, NULL, 'u'},
         {0, 0, 0, 0}
     };
     for (;;) {
@@ -79,6 +124,16 @@ int censorscope_options_init(censorscope_options_t *options,
 
         char *first_invalid;
         switch (c) {
+        case 'd':
+            free(options->download_transport);
+            options->download_transport = strdup(optarg);
+            if (!options->download_transport) {
+                perror("strdup");
+                censorscope_options_destroy(options);
+                return -1;
+            }
+            break;
+
         case 'h':
             print_usage(argv[0]);
             censorscope_options_destroy(options);
@@ -123,10 +178,30 @@ int censorscope_options_init(censorscope_options_t *options,
             }
             break;
 
+        case 'r':
+            free(options->results_dir);
+            options->results_dir = strdup(optarg);
+            if (!options->results_dir) {
+                perror("strdup");
+                censorscope_options_destroy(options);
+                return -1;
+            }
+            break;
+
         case 's':
             free(options->sandbox_dir);
             options->sandbox_dir = strdup(optarg);
             if (!options->sandbox_dir) {
+                perror("strdup");
+                censorscope_options_destroy(options);
+                return -1;
+            }
+            break;
+
+        case 'u':
+            free(options->upload_transport);
+            options->upload_transport = strdup(optarg);
+            if (!options->upload_transport) {
                 perror("strdup");
                 censorscope_options_destroy(options);
                 return -1;
@@ -145,5 +220,8 @@ int censorscope_options_init(censorscope_options_t *options,
 int censorscope_options_destroy(censorscope_options_t *options) {
     free(options->luasrc_dir);
     free(options->sandbox_dir);
+    free(options->results_dir);
+    free(options->download_transport);
+    free(options->upload_transport);
     return 0;
 }
--- src/options.h
+++ src/options.h
@@ -6,8 +6,20 @@
 typedef struct {
     char *sandbox_dir;
     char *luasrc_dir;
+    char *results_dir;
+    /* The total amount of memory available to the interpreter for evaluating
+     * the environment and running the sandboxed code. */
     size_t max_memory;
+    /* The total number of instructions available to evaluate both the
+     * environment and the script itself. Note that is the count of Lua
+     * interpreter instructions, not CPU instructions. If a script can call out
+     * to C functions, this instructions limitation will not gauarantee that the
+     * script completes within a fixed amount of time, especially if sleeping or
+     * waiting on I/O is possible. In that case you should enforce operating
+     * system resource limits. */
     long max_instructions;
+    char *download_transport;
+    char *upload_transport;
 } censorscope_options_t;
 
 int censorscope_options_init(censorscope_options_t *options,
