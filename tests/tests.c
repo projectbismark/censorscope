b8da17a58b738f35447cce67b175d715664b49db
Sam Burnett <srburnet@gmail.com>
2013-11-23 16:38:28 -0500

    Add simple test for is_valid_module_name.

---
+++ tests/tests.c
@@ -0,0 +1,34 @@
+#include "tinytest.h"
+#include "tinytest_macros.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../src/util.h"
+
+void test_is_valid_module_name(void *ptr) {
+    tt_int_op(is_valid_module_name("validmodule"), ==, 1);
+
+    tt_int_op(is_valid_module_name("/usr/lib/lua/invalid.lua"), ==, 0);
+    tt_int_op(is_valid_module_name("../luasrc/api.lua"), ==, 0);
+    tt_int_op(is_valid_module_name("subdir/file.lua"), ==, 0);
+
+end:
+    ;
+}
+
+struct testcase_t censorscope_tests[] = {
+    { "is_valid_module_name", test_is_valid_module_name },
+
+    END_OF_TESTCASES
+};
+
+struct testgroup_t groups[] = {
+    { "censorscope/", censorscope_tests },
+
+    END_OF_GROUPS
+};
+
+int main(int argc, const char **argv) {
+    return tinytest_main(argc, argv, groups);
+}
